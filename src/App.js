74094fffa7d23f56f8af8caa3d3f601083fa7825
Hayat <hayattalaat50@gmail.com>
2021-02-19 14:10:12 +0200

    modules structure

--- src/App.js
+++ src/App.js
@@ -1,22 +1,6 @@
 import React from "react";
-import JumboData from "./fixtures/jumbo";
-import Jumbotron from "./components/Jumbotron";
+import { JumbotronContainer } from "./containers/Jumbotron";
 
 export default function App() {
-  return (
-    <Jumbotron.Container>
-      {JumboData.map((item) => (
-        <Jumbotron key={item.id} direction={item.direction}>
-          <Jumbotron.Pane>
-            <Jumbotron.Title>{item.title}</Jumbotron.Title>
-            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
-          </Jumbotron.Pane>
-
-          <Jumbotron.Pane>
-            <Jumbotron.Image src={item.image} alt={item.alt} />
-          </Jumbotron.Pane>
-        </Jumbotron>
-      ))}
-    </Jumbotron.Container>
-  );
+  return <JumbotronContainer />;
 }
